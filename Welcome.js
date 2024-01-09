f8932d0272dc51a5335b78f5a5e8a76e12b2f639
vanisri-seenuvasan <124983202+vanisri-seenuvasan@users.noreply.github.com>
2024-01-09 21:05:30 +0530

    Update Welcome.js

--- Welcome.js
+++ Welcome.js
@@ -1,9 +1,31 @@
-import React from 'react'
+// Welcome.js
+
+import React from 'react';
 import { Link } from 'react-router-dom';
-import './App.css';
- const Welcome = () => {
+import './Welcome.css';
+
+const Welcome = () => {
+  const backgroundImageUrl = 'https://thumbs.dreamstime.com/b/cinema-background-film-reel-popcorn-drink-tickets-vector-85106601.jpg';
+
+  const containerStyle = {
+    backgroundImage: `url(${backgroundImageUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    position: 'relative',
+    width:'2300px',
+  };
+
   return (
-    <Link to="/movies" className="brand-text">BOOKMYSHOW</Link>
-  )
+    <div className="welcome-container" style={containerStyle}>
+      <div className="content">
+      <div className="book-your-show">BOOK YOUR SHOW</div>
+        <div className="login-button-container">
+          <Link to="/login" className="login-button">Login</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Welcome;
